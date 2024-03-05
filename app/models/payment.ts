8c61e31c2cc65541061f150f787cabb3671e2d92
YamiKawa1 <jehanzao@gmail.com>
2024-03-05 15:03:37 -0300

    add payment model

--- app/models/payment.ts
+++ app/models/payment.ts
@@ -5,6 +5,27 @@ export default class Payment extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
 
+  @column()
+  declare payment_platform_id: number
+
+  @column()
+  declare bill_id: number
+
+  @column()
+  declare user_id: number
+
+  @column()
+  declare payment_id: number
+
+  @column()
+  declare total: number
+
+  @column()
+  declare status: string
+
+  @column()
+  declare address: string
+
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
