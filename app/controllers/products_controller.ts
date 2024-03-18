af50a437b88e6edd5d35f4ccbd94214f1862c2db
YamiKawa1 <jehanzao@gmail.com>
2024-03-18 14:32:45 -0300

    save product

--- app/controllers/products_controller.ts
+++ app/controllers/products_controller.ts
@@ -1,25 +1,68 @@
-// import type { HttpContext } from '@adonisjs/core/http'
-
-import { HttpContext } from "@adonisjs/core/build/modules/http/main";
+import type { HttpContext } from '@adonisjs/core/http'
+import Product from '#models/product'
+import Category from '#models/category'
+import db from '@adonisjs/lucid/services/db'
+import { ApiResponse } from '../utilities/responses.js'
+const res = new ApiResponse()
 
 export default class ProductsController {
-    async product_list(ctx: HttpContext){
+    async list({ response } : HttpContext){
+        try {
+            const product = await Product.all()
+            if (product.length === 0) 
+                return response.status(200).send(res.inform('No hay productos'))         
+
+            return response.status(200).send(res.provide(product, 'Lista de productos'))    
+        
+        } catch (error) {
+            response.status(500).send(res.unexpected())
+            console.log(error);
+        }
+    }
+
+    async find_by_id({ request, response }: HttpContext){
 
     }
 
-    async product_by_id(ctx: HttpContext){
+    async search_by_name({ request, response }: HttpContext){
 
     }
 
-    async add_product(ctx: HttpContext){
+
+    async add({ request, response }: HttpContext){
+        try {
+            const to_add = request.body()
+            console.log(to_add);
+            
+            const category = await Category.find(to_add.category_id)
+            if (!category) {
+                return response.status(400).send(res.inform(`La categoria ${to_add.category_id} no fue encontrada`))
+            }
+
+            const saved = await db
+            .table('products')
+            .returning(['id', 'name'])
+            .insert({
+                name: to_add.name,
+                category_id: category.id,
+                available_quantity: to_add.available_quantity,
+                total_quantity: to_add.available_quantity,
+                price: to_add.price
+            })
+            
+            return response.status(200).send(res.provide(saved, `El producto ${saved.name} fue guardado correctamente bajo el id ${saved.id}`))    
+            
+        } catch (error) {
+            response.status(500).send(res.unexpected())
+            console.log(error);
+        }
+    }
+
+    async delete(ctx: HttpContext){
 
     }
 
-    async delete_product(ctx: HttpContext){
-
-    }
-
-    async update_product(ctx: HttpContext){
+    async update(ctx: HttpContext){
 
     }
 }
